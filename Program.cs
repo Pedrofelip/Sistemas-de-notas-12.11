8da7c2ccde3f2c5e6e6026174514f80d0d30d955
unknown <pedrofelipbarros@gmail.com>
2021-01-07 15:32:32 -0300

    Sistema de notas finalizado Aula dia 12.11

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace estruturas_condicionais_12_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adicione o nome do aluno:");
+            string nomeAluno = Console.ReadLine();
+            Console.WriteLine("---------------------------------");
+            Console.Write("adicione a primeira nota:");
+            float nota1 = float.Parse(Console.ReadLine());
+            Console.Write("adicione a segunda nota:");
+            float nota2 = float.Parse(Console.ReadLine());
+            Console.Write("adicione a terceira nota:");
+            float nota3 = float.Parse(Console.ReadLine());
+            float media = (nota1+nota2+nota3)/3;
+            Console.WriteLine("---------------------------------");
+            if (media>5) {Console.WriteLine("O aluno "+nomeAluno+" foi aprovado com a media de "+media);}
+            else{Console.WriteLine("O aluno "+nomeAluno+" foi reprovado com a media de "+media);}
+
+        }
+    }
+}
